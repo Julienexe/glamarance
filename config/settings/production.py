e8bf2603173b0348b89288d70631a457402fe295
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-01-26 17:01:55 +0300

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
